6c01347e4b4a4a48fd3fe7cb435988480ddaeb8d
RevinooSava <149389117+HEYHAYHOY@users.noreply.github.com>
2026-09-11 07:21:50 +0700

    Add imports for data visualization libraries

--- dashboard/streamlit_dashboard.py
+++ dashboard/streamlit_dashboard.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
 
 st.set_page_config(page_title="Dashboard Produk", layout="wide")
 st.title("Dashboard Penjualan Produk Di Sao Paulo")
